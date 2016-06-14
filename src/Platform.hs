6d378cabba68f29d4116d4cd27833adb4f4d1eed
Marc Scholten <marc@pedigital.de>
2016-06-14 17:14:25 +0200

    Removed database support

--- src/Platform.hs
+++
@@ -1,7 +0,0 @@
-module Platform where
-    import Data.Text
-
-    data Platform = Platform {
-            id :: Int,
-            name :: Text
-        }
--- src/Problem.hs
+++
@@ -1,8 +0,0 @@
-module Problem where
-    import Data.Text
-
-    data Problem = Problem {
-            id :: Int,
-            platformId :: Int,
-            name :: Text
-        }
